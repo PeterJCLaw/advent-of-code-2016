655863850b1eb7daa2598e5b409f12f75350d5d5
Peter Law <PeterJCLaw@gmail.com>
2016-12-11 19:46:19 +0000

    Solve day 3 part 2

--- day_3.rs
+++ day_3.rs
@@ -20,14 +20,49 @@ fn main()
 {
     let stdin = io::stdin();
     let mut n_possible = 0;
+
+    let mut rows = Vec::new();
+
     for line in stdin.lock().lines()
     {
-        let mut numbers = get_numbers(line.unwrap());
+        rows.push(get_numbers(line.unwrap()));
+    }
+
+    let mut maybe_triangles: Vec<Vec<i32>>;
+    if false
+    {
+        // part 1 -- a row is assumed to be a triangle
+        maybe_triangles = rows;
+    }
+    else
+    {
+        // part 2 -- triangles go "downwards"
+        maybe_triangles = Vec::with_capacity(rows.len());
+        for _ in 0..rows.len()
+        {
+            maybe_triangles.push(Vec::with_capacity(3));
+        }
+        for r in 0..(rows.len() / 3)
+        {
+            for r_ in 0..3
+            {
+                let ref row = rows[r * 3 + r_];
+                for c in 0..3
+                {
+                    maybe_triangles[r + c].push(row[c]);
+                }
+            }
+        }
+    }
+
+    for numbers in maybe_triangles
+    {
         //println!("{:?}", numbers);
         if is_valid(numbers)
         {
             n_possible += 1;
         }
     }
+
     println!("{}", n_possible);
 }
