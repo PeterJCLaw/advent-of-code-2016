b4edd707f05c56bc20cbaaefe892eb9d4cfca7af
Peter Law <PeterJCLaw@gmail.com>
2016-12-11 19:20:04 +0000

    Extract helper

--- day_3.rs
+++ day_3.rs
@@ -5,6 +5,17 @@ use advent_of_code::get_numbers;
 use std::io;
 use std::io::BufRead;
 
+fn is_valid(mut numbers: Vec<i32>)
+    -> bool
+{
+    if numbers.len() != 3
+    {
+        return false;
+    }
+    numbers.sort();
+    return numbers[0] >= 0 && numbers[0] + numbers[1] > numbers[2];
+}
+
 fn main()
 {
     let stdin = io::stdin();
@@ -12,8 +23,7 @@ fn main()
     for line in stdin.lock().lines()
     {
         let mut numbers = get_numbers(line.unwrap());
-        numbers.sort();
-        if numbers[0] >= 0 && numbers[0] + numbers[1] > numbers[2]
+        if is_valid(numbers)
         {
             n_possible += 1;
         }
